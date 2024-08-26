9172442e0f2723a8902ffd44efaecfbffd5ecf46
Vishal Singh <vishalsingh@Vishals-MacBook-Air.local>
2024-08-26 20:34:48 +0530

    message

---
+++ script.js
@@ -0,0 +1,43 @@
+function navAnimation(){
+    var nav = document.querySelector("nav")
+
+nav.addEventListener("mouseenter",function(){
+    let tl = gsap.timeline()
+
+   tl.to("#nav-bottom",{
+    height:"20vh"
+   })
+   tl.to(".nav-part2 h5",{
+    display:"block"
+
+   })
+   tl.to(".nav-part2 h5 span",{
+    y:0, 
+    stagger:{
+        amount:0.4
+    }
+
+   })
+})
+
+nav.addEventListener("mouseleave",function(){
+    let  tl = gsap.timeline()
+   
+   tl.to(".nav-part2 h5 span",{
+    y:25,
+    stagger:{
+        amount:0.17
+    }
+   })
+   tl.to(".nav-part2 h5",{
+    display:"none",
+    duration:0.1
+   })
+   tl.to("#nav-bottom",{
+    height:0,
+    duration:0.2
+   })
+
+})
+}
+navAnimation();
