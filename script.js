ec479b38f95e537955a9e7ed03c30b6115e93305
vishalsingh22 <gnsinghvishal7887@gmail,com>
2024-09-03 22:02:01 +0530

    message

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+function loadingAnimation() {
+
+    var tl = gsap.timeline()
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2
+    })
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(0%)",
+        borderRadius: "50px",
+        duration: 2,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1, #page1 p, #page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
+}
+
+
+
 function navAnimation(){
     var nav = document.querySelector("nav")
 
@@ -40,4 +102,127 @@ nav.addEventListener("mouseleave",function(){
 
 })
 }
-navAnimation();
+
+
+
+
+function page2Animation(){
+
+    // this efect only one element of right elem whose id is #right-elem1
+// var relme = document.querySelector("#right-elem1")
+// var relmeImg = document.querySelector("#right-elem1 img")
+
+// relme.addEventListener("mouseenter",function(){
+ 
+//     relmeImg.style.opacity = 1
+
+// })
+
+// for All hover image 
+
+
+    var rightElme = document.querySelectorAll(".right-elem")
+ 
+
+rightElme.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        gsap.to(elem.childNodes[3],{
+            opacity :1,
+            scale:1
+        })
+    })
+    elem.addEventListener("mouseleave", function(){
+        gsap.to(elem.childNodes[3],{
+            opacity : 0,
+            scale:0
+        })
+    })
+    elem.addEventListener("mousemove",function(dets){
+   
+        //console.log(elem.getBoundingClientRect())
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect().x-30,
+            y:dets.y - elem.getBoundingClientRect().y-100
+            
+        })
+
+        
+    })
+
+})
+}
+
+
+
+function page3Animation(){
+    var page3Center = document.querySelector(".page3-center")
+
+var video = document.querySelector("#page3 video")
+
+page3Center.addEventListener("click" , function(){
+    video.play()
+    gsap.to(video , {
+        transfrom:"scaleX(1) scaleY(1)",
+        opacity:1,
+        borderRadius:0
+    })
+})
+
+video.addEventListener("click",function(){
+    video.pause();
+    gsap.to(video , {
+        transfrom:"scaleX(0.7) scaleY(0)",
+        opacity:0,
+        borderRadius:"30px"
+    })
+})
+
+var sections = document.querySelectorAll(".section-right")
+
+sections.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+      
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave", function(){
+      
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
+}
+
+
+function page7Animation(){
+
+
+gsap.from(".bottom7-parts h4",{
+    x:0,
+    duration:0.2,
+    scrollTrigger:{
+        trigger: ".bottom7-parts",
+        scroller: "body",
+        // markers:true,
+        start: "top 80%",
+        end: "top 10%",
+        scrub: true
+    }
+
+})
+}
+
+
+// locomotiveAnimation()
+
+
+
+navAnimation()
+
+page2Animation()
+
+page3Animation()
+
+page7Animation()
+loadingAnimation()
+
